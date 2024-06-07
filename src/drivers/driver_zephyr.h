23ad029ed0dfaef906423c8e27cb91e7a8bf81ba
rex.chen <rex.chen_1@nxp.com>
2024-06-07 18:54:33 +0800

    [nxp noup] zephyr: Add passive ECSA support for zephyr
    Add passive ECSA support for zephyr.
    
    Signed-off-by: rex.chen <rex.chen_1@nxp.com>

--- src/drivers/driver_zephyr.h
+++ src/drivers/driver_zephyr.h
@@ -198,6 +198,10 @@ struct zep_wpa_supp_dev_callbk_fns {
 			union wpa_event_data *event);
 
 	void (*mac_changed)(struct zep_drv_if_ctx *if_ctx);
+
+	void (*ecsa_complete)(struct zep_drv_if_ctx *if_ctx,
+              union wpa_event_data *event);
+
 };
 
 #ifdef CONFIG_WIFI_NM_HOSTAPD_AP
