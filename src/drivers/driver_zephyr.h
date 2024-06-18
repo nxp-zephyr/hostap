5ca29dfdbc0a56a9646f7f02f8fcc1962ac39e30
Fengming Ye <frank.ye@nxp.com>
2024-06-18 12:27:02 +0900

    Issue: WSW-45703 Fix DPP 4.7 DPP-Auth initiation failed no response received
    [Issue]
    Configurator send DPP auth request on channel 1 and need to
    receive DPP auth response on channel 6.
    This requires support of send_action_cancel_wait ops to cancel
    remain on channel after TX success on channel 1, and then
    remain on channel 6.
    But previous code does not support send_action_cancel_wait ops.
    
    [Fix]
    Add send_action_cancel_wait ops support.
    
    [Validation done]
    Compilation successful and sanity test.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Gannicus Guo, Sandip Dalvi, Maochen Wang, Li Long, Hui Bai
    
    Signed-off-by: Fengming Ye <frank.ye@nxp.com>

--- src/drivers/driver_zephyr.h
+++ src/drivers/driver_zephyr.h
@@ -338,6 +338,7 @@ struct zep_wpa_supp_dev_ops {
 	int (*dpp_listen)(void *priv, bool enable);
 	int (*remain_on_channel)(void *priv, unsigned int freq, unsigned int duration);
 	int (*cancel_remain_on_channel)(void *priv);
+	void (*send_action_cancel_wait)(void *priv);
 };
 
 #endif /* DRIVER_ZEPHYR_H */
