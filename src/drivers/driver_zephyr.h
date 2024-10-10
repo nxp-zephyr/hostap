a2284bdf372504f21930f15b2d1398ae76be9a63
Maochen Wang <maochen.wang@nxp.com>
2024-10-10 14:41:35 +0800

    Revert "[nxp noup] zephyr: Add passive ECSA support for zephyr"
    This reverts commit 23ad029ed0dfaef906423c8e27cb91e7a8bf81ba.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- src/drivers/driver_zephyr.h
+++ src/drivers/driver_zephyr.h
@@ -198,10 +198,6 @@ struct zep_wpa_supp_dev_callbk_fns {
 			union wpa_event_data *event);
 
 	void (*mac_changed)(struct zep_drv_if_ctx *if_ctx);
-
-	void (*ecsa_complete)(struct zep_drv_if_ctx *if_ctx,
-              union wpa_event_data *event);
-
 };
 
 #ifdef CONFIG_WIFI_NM_HOSTAPD_AP
