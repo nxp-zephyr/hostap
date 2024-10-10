f67848822b85e3c2f6a0a78b08d5a688dd46c60c
Maochen Wang <maochen.wang@nxp.com>
2024-10-10 14:41:33 +0800

    Revert "[nxp toup] zephyr: fix run udp throughput fail when legacy roaming"
    This reverts commit a691140f0fccd5675dae54f7c21b4b261aa3eea8.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- src/drivers/driver_zephyr.c
+++ src/drivers/driver_zephyr.c
@@ -563,9 +563,6 @@ void wpa_drv_zep_event_proc_assoc_resp(struct zep_drv_if_ctx *if_ctx,
 			wpa_drv_zep_free_pairwise_key_params(if_ctx->key_params);
 			if_ctx->key_params = NULL;
 		}
-		if (if_ctx->roaming) {
-		    if_ctx->roaming = false;
-		}
 		wpa_supplicant_event_wrapper(if_ctx->supp_if_ctx,
 				EVENT_ASSOC_REJECT,
 				event);
@@ -1649,10 +1646,6 @@ static int wpa_drv_zep_deauthenticate(void *priv, const u8 *addr,
 		if_ctx->ft_roaming = false;
 	}
 
-	if (if_ctx->roaming) {
-		if_ctx->roaming = false;
-	}
-
 	dev_ops = get_dev_ops(if_ctx->dev_ctx);
 	ret = dev_ops->deauthenticate(if_ctx->dev_priv, addr, reason_code);
 	if (ret) {
@@ -1673,7 +1666,6 @@ static int wpa_drv_zep_authenticate(void *priv,
 	const struct zep_wpa_supp_dev_ops *dev_ops;
 	struct wpa_bss *curr_bss;
 	int ret = -1;
-	struct wpa_supplicant *wpa_s = NULL;
 
 	if ((!priv) || (!params)) {
 		wpa_printf(MSG_ERROR, "%s: Invalid params", __func__);
@@ -1681,18 +1673,12 @@ static int wpa_drv_zep_authenticate(void *priv,
 	}
 
 	if_ctx = priv;
-	wpa_s = if_ctx->supp_if_ctx;
+
 	if_ctx->ft_roaming = false;
-	if_ctx->roaming = false;
 
 	if (params->auth_alg == WPA_AUTH_ALG_FT) {
 		if_ctx->ft_roaming = true;
 	}
-
-	if (wpa_s->assoc_freq) {
-		if_ctx->roaming = true;
-	}
-
 	dev_ops = get_dev_ops(if_ctx->dev_ctx);
 
 	os_memcpy(if_ctx->ssid, params->ssid, params->ssid_len);
@@ -1950,11 +1936,10 @@ static int wpa_drv_zep_set_supp_port(void *priv,
 	struct zep_drv_if_ctx *if_ctx = NULL;
 	const struct zep_wpa_supp_dev_ops *dev_ops;
 	struct net_if *iface = NULL;
-	struct wpa_supplicant *wpa_s = NULL;
+
 	int ret;
 
 	if_ctx = priv;
-	wpa_s = if_ctx->supp_if_ctx;
 
 	dev_ops = get_dev_ops(if_ctx->dev_ctx);
 
@@ -1966,11 +1951,10 @@ static int wpa_drv_zep_set_supp_port(void *priv,
 
 #ifdef CONFIG_NET_DHCPV4
 	if (authorized) {
-		if (if_ctx->ft_roaming == true || if_ctx->roaming == true) {
-			if_ctx->roaming = false;
-			if_ctx->ft_roaming = false;
-		} else {
+		if (if_ctx->ft_roaming == false) {
 			net_dhcpv4_restart(iface);
+		} else {
+			if_ctx->ft_roaming = false;
 		}
 	}
 #endif
