1273c6713194be3207ea6e0b0090262fc0389132
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-10-14 20:23:36 -0500

    Revert "[nxp noup] Added 11k/v/r roaming support in wpa_supplicant for Zephyr"
    This reverts commit 3441affe52827909b2152e1e45653cd29ff55ee0.

--- src/drivers/driver_zephyr.h
+++ src/drivers/driver_zephyr.h
@@ -137,7 +137,6 @@ struct zep_drv_if_ctx {
 	struct wpa_scan_results *scan_res2;
 	bool scan_res2_get_in_prog;
 
-	bool ft_roaming;
 	unsigned int freq;
 	unsigned char ssid[SSID_MAX_LEN];
 	size_t ssid_len;
