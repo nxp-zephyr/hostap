c8d976e77e6815900ff4717591af4f4c9cb3425c
Maochen Wang <maochen.wang@nxp.com>
2025-01-16 18:14:58 +0800

    Revert "RSNO: Use SNonce cookie to indicate support for RSN overriding"
    This reverts commit 61f0da34c2d91838573826ee01d8d5940f274ed1.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- src/rsn_supp/wpa.c
+++ src/rsn_supp/wpa.c
@@ -1023,8 +1023,6 @@ static void wpa_supplicant_process_1_of_4(struct wpa_sm *sm,
 				"WPA: Failed to get random data for SNonce");
 			goto failed;
 		}
-		if (sm->rsn_override != RSN_OVERRIDE_NOT_USED)
-			rsn_set_snonce_cookie(sm->snonce);
 		sm->renew_snonce = 0;
 		wpa_hexdump(MSG_DEBUG, "WPA: Renewed SNonce",
 			    sm->snonce, WPA_NONCE_LEN);
