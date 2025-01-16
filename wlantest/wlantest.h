e854de0a18062a115b84b079977c4fbfad71660e
Maochen Wang <maochen.wang@nxp.com>
2025-01-16 18:14:57 +0800

    Revert "wlantest: Use AP's RSNXOE for capabilities when RSNO is used"
    This reverts commit 495f3097c3c1a4a1aecde41e45a40e9ff2402874.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- wlantest/wlantest.h
+++ wlantest/wlantest.h
@@ -121,8 +121,6 @@ struct wlantest_sta {
 
 	u16 sae_group;
 	u16 owe_group;
-
-	enum rsn_selection_variant rsn_selection;
 };
 
 struct wlantest_tdls {
@@ -160,8 +158,6 @@ struct wlantest_bss {
 	u8 rsnie[257];
 	u8 rsnxe[254]; /* RSNXE data */
 	size_t rsnxe_len;
-	u8 rsnxoe[251]; /* RSNXOE data */
-	size_t rsnxoe_len;
 	u8 osenie[257];
 	int proto;
 	int pairwise_cipher;
