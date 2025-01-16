f02c09d5cc3c0eb2bc3e825154cc9ae02ab0dbca
Maochen Wang <maochen.wang@nxp.com>
2025-01-16 18:14:57 +0800

    Revert "RSNO: Allow RSN overriding to be enabled for a specific network"
    This reverts commit 60eb4b6b2ec02c52efe19c38c9d00a1d778d0554.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- wpa_supplicant/config.h
+++ wpa_supplicant/config.h
@@ -1775,9 +1775,17 @@ struct wpa_config {
 	int wowlan_disconnect_on_deinit;
 
 	/**
-	 * rsn_overriding - RSN overriding (default behavior)
+	 * rsn_overriding - RSN overriding
+	 *
+	 * 0 = Disabled
+	 * 1 = Enabled automatically if the driver indicates support
+	 * 2 = Forced to be enabled even without driver capability indication
 	 */
-	enum wpas_rsn_overriding rsn_overriding;
+	enum rsn_overriding {
+		RSN_OVERRIDING_DISABLED = 0,
+		RSN_OVERRIDING_AUTO = 1,
+		RSN_OVERRIDING_ENABLED = 2,
+	} rsn_overriding;
 
 #ifdef CONFIG_PASN
 #ifdef CONFIG_TESTING_OPTIONS
