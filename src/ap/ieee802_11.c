77f97ba4e432a496c5aac9c35b822c8bd46276a4
Maochen Wang <maochen.wang@nxp.com>
2025-01-23 11:00:39 +0800

    Revert "RSNO: Omit RSNXE in (Re)Association Response frame like in Beacon frame"
    This reverts commit a1f473129a14f4ae7c7a4665fc5b95539fb0a5bd.
    
    Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -5586,8 +5586,6 @@ static void handle_assoc(struct hostapd_data *hapd,
 	if (resp != WLAN_STATUS_SUCCESS)
 		goto fail;
 	omit_rsnxe = !get_ie(pos, left, WLAN_EID_RSNX);
-	if (hapd->conf->rsn_override_omit_rsnxe)
-		omit_rsnxe = 1;
 
 	if (hostapd_get_aid(hapd, sta) < 0) {
 		hostapd_logger(hapd, mgmt->sa, HOSTAPD_MODULE_IEEE80211,
